23b26f493b1fb348036743bef043a49d118c6cb1
novellus <novellus.cato@gmail.com>
2021-01-06 02:13:19 -0800

    simplifying validation error summary. outlining remainin project steps.

--- 02_dispo_validation_failures.py
+++ 02_dispo_validation_failures.py
@@ -14,8 +14,31 @@ if __name__ == '__main__':
 
     print('iterating manifest')
 
-    tracebacks = defaultdict(list)
-    tracebacks_reverse = defaultdict(list)
+    errors_by_app = defaultdict(list)  # appid -> error codes
+    errors_by_code = defaultdict(list)  # error code -> appids
+
+    date_parse_error_code = 'ValueError: All date formats failed to parse date string, '  # use as key to log date strings for display
+    date_string_summary = []
+
+    consolidate_error_codes = [
+        'AssertionError: details success is false',
+        'AssertionError: data is empty',
+        'AssertionError: release_date is empty',
+        'AssertionError: release_date -> date is empty',
+        'AssertionError: name field is empty',
+        'AssertionError: appid does not match queried appid',
+        'AssertionError: reviews success is false',
+        'AssertionError: query_summary does not exist',
+        'AssertionError: query_summary -> total_positive is not an int',
+        'AssertionError: query_summary -> total_positive is negative',
+        'AssertionError: query_summary -> total_negative is not an int',
+        'AssertionError: query_summary -> total_negative is negative',
+        'AssertionError: query_summary -> total_reviews is not an int',
+        'AssertionError: query_summary -> total_reviews is negative',
+        'AssertionError: total reviews does not equal sum of positive and negative reviews',
+        date_parse_error_code,
+        'json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)',
+    ]
 
     for app in manifest['applist']['apps']:
         appid = app['appid']
@@ -32,20 +55,30 @@ if __name__ == '__main__':
                     traceback = f.read()
                     f.close()
 
-                    tracebacks[appid].append(traceback)
+                    error_code = traceback
+                    for err in consolidate_error_codes:
+                        if err in traceback:
+                            error_code = err  # use shorthand since full traceback varies as line numbers evolve in sourcecode
+                            if err == date_parse_error_code:
+                                date_string = re.search(f'{re.escape(date_parse_error_code)}([^\n]*)\n', traceback).group(1)
+                                date_string_summary.append(date_string)
+
+                    errors_by_app[appid].append(error_code)
 
             # multiple compare entries for consistency
             # do not log repeated tracebacks more than once
             # log inconsistent tracebacks under a special category
-            num_unique_tracebacks = len(set(tracebacks[appid]))
+            num_unique_tracebacks = len(set(errors_by_app[appid]))
             if num_unique_tracebacks == 1:
-                tracebacks_reverse[traceback].append(appid)
+                errors_by_code[error_code].append(appid)
             else:
-                tracebacks_reverse['multiple_unique_tracebacks'].append(appid)
+                errors_by_code['multiple_unique_tracebacks'].append(appid)
 
-    f = open('02_validation_failures_summary.txt', 'w')
-    for traceback, appids in sorted(tracebacks_reverse.items(), key=lambda x: -len(x[1])):  # largest number of offenders listed first
-        f.write(f'--A-- {len(appids)} offending apps\n')
+    f = open('02_validation_failures_summary.txt', 'w', encoding='utf-8')
+    for error_code, appids in sorted(errors_by_code.items(), key=lambda x: -len(x[1])):  # largest number of offenders listed first
+        f.write(f'{len(appids)} offending apps\n')
         f.write(f'{appids}\n')
-        f.write(traceback)
+        f.write(error_code)
+        if error_code == date_parse_error_code:
+            f.write(f'{sorted(list(set(date_string_summary)))}')
         f.write('\n\n\n\n')
